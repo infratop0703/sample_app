9999aacf7a092a26e1fe866166dd64a37e5c43be
EC2 Default User <ec2-user@ip-172-31-14-67.ec2.internal>
2022-08-15 06:47:53 +0000

    [Update]5-更新機能レイアウト

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   get 'lists/new'
-  get 'lists/edit'
   get '/top' => 'homes#top'
   post 'lists' => 'lists#create'
   get 'lists' => 'lists#index'
   # list毎の詳細画面
-  get 'list/:id' => 'lists#show', as: "list" #'lists#showの設定をlistとして'
+  get 'list/:id' => 'lists#show', as: "list" #'lists#showの設定をlistとして利用できる'
+  get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
