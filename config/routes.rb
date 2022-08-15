2cf0beee89e75a69cb814229c8d6ba06556105ce
EC2 Default User <ec2-user@ip-172-31-14-67.ec2.internal>
2022-08-15 07:39:12 +0000

    [Finish]7-ルーティンの一括実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,6 @@
 Rails.application.routes.draw do
   get 'lists/new'
   get '/top' => 'homes#top'
-  # 新規投稿はpost
-  post 'lists' => 'lists#create'
-  get 'lists' => 'lists#index'
-  # list毎の詳細画面
-  get 'list/:id' => 'lists#show', as: "list" #'lists#showの設定をlistとして利用できる'
-  get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
-  # 更新はpatch
-  patch 'lists/:id' => 'lists#update', as: 'update_list'
-  # 削除はdelete
-  delete 'list/:id' => 'lists#destroy', as: 'destroy_list'
+  resources :lists #ルーティンの
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
